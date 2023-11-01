c708ca2e5c2815dcbf1ce97f0c1eef0f4aa4f611
roxstarprasad <149354096+roxstarprasad@users.noreply.github.com>
2023-11-02 00:51:40 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,62 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String dept;
+    private double salary;
+
+    public Employee(int id, String name, String dept, double salary) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Employee employee = new Employee(1, "Prasad", "Sales", 25000);
+        System.out.println(employee.toString());
+    }
+}
